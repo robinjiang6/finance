acbdf537287b43eecf5ca81d95a9ac14e980722a
robinjiang6 <robinjiang6@gmail.com>
2023-08-01 00:10:26 -0400

    unit tested get methods in UserInput class.

--- tests/logic/test_user_input.py
+++ tests/logic/test_user_input.py
@@ -11,17 +11,19 @@ from unittest.mock import patch
 
 
 def run_with_io(func):
-    """Decorator that runs func with contextlib.redirect_stdout and patch to simulate standard input and output."""
+    """Decorator that runs func with contextlib.redirect_stdout and patch to simulate standard input and output.
+    new function returns a tuple of length 2, with a list at index = 0 containing standard output from the program
+    and whatever the original function outputted at index = 1."""
     def new_func(*args, standard_inputs: list = None, **kwargs):
         if standard_inputs is None:
             with contextlib.redirect_stdout(io.StringIO()) as output:
-                func(*args, **kwargs)
-            return output.getvalue()
+                func_return = func(*args, **kwargs)
+            return output.getvalue(), func_return
         else:
             with contextlib.redirect_stdout(io.StringIO()) as output:
                 with patch('builtins.input', side_effect=standard_inputs):
-                    func(*args, **kwargs)
-            return output.getvalue()
+                    func_return = func(*args, **kwargs)
+            return output.getvalue(), func_return
 
     return new_func
 
@@ -31,27 +33,61 @@ class TestRunWithIO(unittest.TestCase):
         @run_with_io
         def multiply(x, y):
             print(x * y)
+            return x * y
 
-        self.assertEqual('12', multiply(3, 4).strip())
+        self.assertEqual('12', multiply(3, 4)[0].strip())
+        self.assertEqual(12, multiply(3, 4)[1])
 
     def test_run_works_with_keyword_arguments(self):
         @run_with_io
         def multiply(*, first, second):
             print(first * second)
+            return first * second
 
-        self.assertEqual('12', multiply(first=3, second=4).strip())
+        self.assertEqual('12', multiply(first=3, second=4)[0].strip())
+        self.assertEqual(12, multiply(first=3, second=4)[1])
 
     def test_run_works_with_inputs(self):
         @run_with_io
         def multiply():
-            print(int(input()) * int(input()))
+            first = int(input())
+            second = int(input())
+            print(first * second)
+            return first * second
 
-        self.assertEqual('12', multiply(standard_inputs=[3, 4]).strip())
+        self.assertEqual('12', multiply(standard_inputs=[3, 4])[0].strip())
+        self.assertEqual(12, multiply(standard_inputs=[3, 4])[1])
 
 
 class TestUserInput(unittest.TestCase):
-    def test_something(self):
-        pass
+    def test_get_ticker_symbol(self):
+        @run_with_io
+        def outside_func():
+            return UserInput.get_ticker_symbol()
+
+        self.assertEqual("AAPL", outside_func(standard_inputs=["AAPL"])[1])
+        self.assertEqual("12345", outside_func(standard_inputs=["123456", "12345", "AAPL"])[1])
+
+    def test_get_buy_year(self):
+        @run_with_io
+        def outside_func():
+            return UserInput.get_buy_year()
+
+        self.assertEqual(1701, outside_func(standard_inputs=["1701"])[1])
+        self.assertEqual(2023, outside_func(standard_inputs=["2023"])[1])
+        self.assertEqual(1850, outside_func(standard_inputs=["1850"])[1])
+        self.assertEqual(1701, outside_func(standard_inputs=["1699", "1700", "2025", "1701"])[1])
+
+    def test_get_dollar_amount(self):
+        @run_with_io
+        def outside_func():
+            return UserInput.get_dollar_amount()
+
+        self.assertEqual(1701, outside_func(standard_inputs=["1701"])[1])
+        self.assertEqual(2023, outside_func(standard_inputs=["2023"])[1])
+        self.assertEqual(1850, outside_func(standard_inputs=["1850"])[1])
+        self.assertEqual(1699, outside_func(standard_inputs=["1699", "1700", "2025", "1701"])[1])
+        self.assertEqual(1701, outside_func(standard_inputs=["abdfb", "@#$dg", "202df5", "1701"])[1])
 
 
 if __name__ == "__main__":
