a7fe3c53bbdb839fef320e1081be73c64747f258
robinjiang6 <robinjiang6@gmail.com>
2023-08-01 20:03:21 -0400

    Started working on finance_driver module, took notes in finance_notes on how yfinance Ticker and panda Dataframe work.

---
+++ logic/finance_driver.py
@@ -0,0 +1,9 @@
+# finance_driver.py
+# Author: Robin Jiang
+#
+#
+# This module is for using the yahoo finance API to search
+# for stock prices for a given StockSearch (from user_input)
+
+import yfinance
+
