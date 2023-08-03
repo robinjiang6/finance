aaa9e4260b88ca11b7f01e151ff75e15a0e724b5
robinjiang6 <robinjiang6@gmail.com>
2023-08-03 16:39:45 -0400

    Started implementing calculations for monthly investment.

--- logic/finance_driver.py
+++ logic/finance_driver.py
@@ -25,35 +25,63 @@ class CalculateReturn:
         if self._info is None:
             raise TickerError(f'"{search.symbol}" is not a valid symbol.')
 
+    def __getitem__(self, key):
+        """Returns the item stored in self._info at key, or raises KeyError if key does not exist."""
+        return self._info[key]
+
     def get_returns(self) -> float:
         """Uses the information in search to calculate change in capital based on yfinance."""
 
-        month, day = _format_month_and_day()
+        month, next_month, day = _format_month_and_day()
 
         # search for one year from search.year to search.year + 1
-        history = self._ticker.history(start=f"{self._search.year}-{month}-{day}",
-                                       end=f"{self._search.year + 1}-{month}-{day}")
+        history = self._get_history(self._search.year, month, day, end_year=self._search.year + 1)
+
         if 'currentPrice' not in self._info:
             current_price = self._info['previousClose']
         else:
             current_price = self._info['currentPrice']
 
-        return self.calculate_change(history.iloc[0]["Open"], current_price, self._search.dollars)
+        return self.calculate_change(history.iloc[0]["Open"], current_price)
 
-    def calculate_change(self, starting_price: int | float, ending_price: int | float, amount_invested: int | float) -> float:
+    def calculate_change(self, starting_price: int | float, ending_price: int | float) -> float:
         """Returns the amount of money resulting after investing at starting_price."""
-        return round(amount_invested * (ending_price / starting_price), 2)
+        if self._search.monthly_investment == 0:
+            return round(self._search.principal_investment * (ending_price / starting_price), 2)
+        else:
+            # WORK ON THIS
+            pass
 
 
-def _format_month_and_day() -> tuple[str | int, str | int]:
-    """returns a tuple of month, day in (DD, YY) form"""
+    def _get_history(self, year: int, month: int | str, day: int | str, end_year: int = None, end_month: int | str = None) -> 'pandas.Dataframe':
+        """Gets the history of a stock from the given date. If no end date is given, 1 month later will be chosen.
+        Month and day must be either double-digit or in 0X form"""
+
+        if end_month is None:
+            end_month = month
+        if end_year is None:
+            if end_month != month and end_month == 1:
+                end_year = year + 1
+            else:
+                end_year = year
+        return self._ticker.history(start=f"{year}-{month}-{day}",
+                                    end=f"{end_year}-{end_month}-{day}")
+
+
+def _format_month_and_day() -> tuple[str | int, str | int, str | int]:
+    """returns a tuple of month, next_month, and day in (MM, MM, DD) form"""
     month = datetime.date.today().month
+    next_month = month + 1
+    if next_month == 13:
+        next_month = 1
     day = datetime.date.today().day
     if month < 10:
         month = '0' + str(month)
+    if next_month < 10:
+        next_month = '0' + str(next_month)
     if day < 10:
         day = '0' + str(day)
-    return month, day
+    return month, next_month, day
 
 
 __all__ = [TickerError.__name__, CalculateReturn.__name__]
--- shell_driver.py
+++ shell_driver.py
@@ -3,14 +3,15 @@
 #
 # This module drives the shell version of the app
 
-from logic.finance_driver import get_change_in_capital
-from logic.user_input import UserInput
+from logic.finance_driver import CalculateReturn
+from logic.user_input import get_user_input
 
 
 def run() -> None:
-    search = UserInput().get_user_input()
-    print(f"If you had invested ${search.dollars} into {search.symbol} in "
-          f"{search.year}, you would now have ${get_change_in_capital(search)}")
+    search = get_user_input()
+    s = CalculateReturn(search)
+    print(f"If you had invested ${search.principal_investment} into {search.symbol} in "
+          f"{search.year} with a monthly investment of ${search.monthly_investment}, you would now have ${s.get_returns()}")
 
 
 if __name__ == "__main__":
