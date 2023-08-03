e1311e39e259dbfb404f568d9e0d218f8be914f9
robinjiang6 <robinjiang6@gmail.com>
2023-08-03 15:48:59 -0400

    Changed two methods to be part of a class.
    Will change calculation to allow for monthly investment, as well as to be able to index ticker.info.

--- logic/finance_driver.py
+++ logic/finance_driver.py
@@ -15,29 +15,34 @@ class TickerError(Exception):
     pass
 
 
-def get_change_in_capital(search: StockSearch) -> float:
-    """Uses the information in search to calculate change in capital based on yfinance."""
-    ticker = yfinance.Ticker(search.symbol)
-    if ticker.info is None:
-        raise TickerError(f'"{search.symbol}" is not a valid symbol.')
+class CalculateReturn:
+    """class that calculates return on investment"""
 
-    month, day = _format_month_and_day()
+    def __init__(self, search: StockSearch):
+        self._search = search
+        self._ticker = yfinance.Ticker(search.symbol)
+        self._info = self._ticker.info
+        if self._info is None:
+            raise TickerError(f'"{search.symbol}" is not a valid symbol.')
 
-    # search for one year from search.year to search.year + 1
-    history = ticker.history(start=f"{search.year}-{month}-{day}",
-                             end=f"{search.year + 1}-{month}-{day}")
-    info = ticker.info
-    if 'currentPrice' not in info:
-        current_price = info['previousClose']
-    else:
-        current_price = info['currentPrice']
+    def get_returns(self) -> float:
+        """Uses the information in search to calculate change in capital based on yfinance."""
 
-    return calculate_change(history.iloc[0]["Open"], current_price, search.dollars)
+        month, day = _format_month_and_day()
 
+        # search for one year from search.year to search.year + 1
+        history = self._ticker.history(start=f"{self._search.year}-{month}-{day}",
+                                       end=f"{self._search.year + 1}-{month}-{day}")
+        if 'currentPrice' not in self._info:
+            current_price = self._info['previousClose']
+        else:
+            current_price = self._info['currentPrice']
 
-def calculate_change(starting_price: int | float, ending_price: int | float, amount_invested: int | float) -> float:
-    """Returns the amount of money resulting after investing at starting_price."""
-    return round(amount_invested * (ending_price/starting_price), 2)
+        return self.calculate_change(history.iloc[0]["Open"], current_price, self._search.dollars)
+
+    def calculate_change(self, starting_price: int | float, ending_price: int | float, amount_invested: int | float) -> float:
+        """Returns the amount of money resulting after investing at starting_price."""
+        return round(amount_invested * (ending_price / starting_price), 2)
 
 
 def _format_month_and_day() -> tuple[str | int, str | int]:
@@ -51,4 +56,4 @@ def _format_month_and_day() -> tuple[str | int, str | int]:
     return month, day
 
 
-__all__ = [TickerError.__name__, get_change_in_capital.__name__, calculate_change.__name__]
+__all__ = [TickerError.__name__, CalculateReturn.__name__]
